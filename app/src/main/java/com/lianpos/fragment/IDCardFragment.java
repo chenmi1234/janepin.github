c7621d19a8ce9155a8b8b2f1bde867d708d4710d
chenmi1234 <dalian_shmily@163.com>
2017-11-20 17:21:23 +0800

    身份卡 里面的品牌

--- app/src/main/java/com/lianpos/fragment/IDCardFragment.java
+++ app/src/main/java/com/lianpos/fragment/IDCardFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.lianpos.activity.R;
@@ -22,9 +24,17 @@ import com.lianpos.util.JumpUtil;
  */
 public class IDCardFragment extends Fragment {
 
+	private LinearLayout no_brand;
+	private RelativeLayout frist_brand;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_idcard, null);
+		no_brand = (LinearLayout) rootView.findViewById(R.id.no_brand);
+		frist_brand = (RelativeLayout) rootView.findViewById(R.id.frist_brand);
+		if (frist_brand.getVisibility() == View.VISIBLE){
+			no_brand.setVisibility(View.GONE);
+		}
 		return rootView;
 	}
 }
